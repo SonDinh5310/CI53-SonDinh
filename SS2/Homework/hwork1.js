54ea0acc685561ca5307992fa8383a9973124cca
Son Dinh <dnson20022001@gmail.com>
2020-11-27 16:48:59 +0700

    commit code

---
+++ SS2/Homework/hwork1.js
@@ -0,0 +1,18 @@
+//Bai 1: Tìm hiểu về module trong javascript? Import/Export? Cho 1 ví dụ?
+/*
+* Khai niem: Module trong JS la bao gom mot tap hop cac ham de giup xu ly 1 chuc nang
+nao do. Cac chuc nang khac nhau co the co cac Module khac nhau. Cac ham trong Module co
+the duoc import de su dung binh thuong.
+
+* Tai sao lai su dung module?
++Tinh bao tri
++Namespacing
++Tai su dung duoc
+
+* Import/Export
+-Import: duoc su dung de lay cac ham, doi tuong hoac gia tri duoc export trong module de
+dua vao su dung trong chuong trinh.
+-Export: duoc su dung khi tao cac module JS de goi cac ham, doi tuong hoac gia tri trong
+module de chung co the duoc su dung boi cac chuong trinh bang lenh import.
+
+*/
